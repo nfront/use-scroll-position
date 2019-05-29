d1153a66f076ecf6b34f87207efba639b83c292f
Magnus Rinnan Gaarder <magnus.rinnan@gmail.com>
2019-05-29 02:18:29 +0200

    test: pass with no tests

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   verbose: true,
-  testRegex: '(/__tests__/.*|(\.|/)(test|spec|ispec))\.[jt]sx?$',
+  testRegex: '(/__tests__/.*|(.|/)(test|spec|ispec)).[jt]sx?$',
   collectCoverage: true,
   collectCoverageFrom: ['./src/**/*.{js,jsx}', '!**/node_modules/**', '!**/vendor/**'],
   coverageDirectory: './coverage',
